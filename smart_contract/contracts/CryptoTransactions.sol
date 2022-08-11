c2ca3eacf686ed479606e311584a69cf7a8ffb97
OsaeAddo <immanueloaddo@gmail.com>
2022-08-11 08:54:45 +0000

    create a contract object for transactions

---
+++ smart_contract/contracts/CryptoTransactions.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+
+pragma solidity ^0.8.9;
+
+//transactions object
+contract CryptoTransactions {
+    uint256 transactionCount; //track no. of transactions
+
+    //define function for transfering the crypto
+    event Transfer(address from, address recipient, uint256 amount, string message, uint256 timestamp, string keyword);
+
+    //structure of the transfer
+    struct TransferStruct {
+        address sender;
+        address recipient;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    
+}
