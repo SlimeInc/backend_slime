9b13739c684008b9e0fcb0cebe78b58addb5582f
OsaeAddo <immanueloaddo@gmail.com>
2022-09-01 07:55:28 +0000

    update deploy.js

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -23,7 +23,19 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+runMain()
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
