4b8d82b67008879f75d69d0fc85ba4708a9649e6
OsaeAddo <immanueloaddo@gmail.com>
2022-09-01 15:20:46 +0000

    define network for smart contracts to be deployed to, Goerli was used

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-};
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/8fXJQjIjK8DahGL7SnROshHz1zXzXLFG',
+      accounts: ['1cdf28df2bc63ecec20b47e65de4e05508a5529188d2a65627fc05db645c2d20'] //priv. key of metamask acct to be used for deployment
+    }
+  }
+}
